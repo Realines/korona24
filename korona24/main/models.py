987dd678cf14bcbcb8130b7e5d9187d4c5b4535d
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2021-12-10 04:15:16 +0700

    Added Gallery and Consultation models.

--- korona24/main/models.py
+++ korona24/main/models.py
@@ -3,4 +3,41 @@ from django.utils.translation import gettext_lazy as _
 from django.core import validators
 
 
+class Consultation(models.Model):
+    client_name = models.CharField(
+        max_length=128,
+        verbose_name=_('Имя клиента'),
+    )
+    phone_number = ... # TODO: Найти хороший способ валидации номеров.
+    date_added = models.DateField(
+        verbose_name=_('Дата заявки'),
+        auto_now_add=True,
+    )
 
+    class Meta:
+        verbose_name = _('Консультация')
+        verbose_name_plural = _('Консультации')
+        ordering = ['-date_added']
+        get_latest_by = 'date_added'
+
+    def __str__(self) -> str:
+        return f'{self.client_name}#{self.pk}'
+
+
+class Gallery(models.Model):
+    image = models.ImageField(
+        upload_to='gallery/',
+        verbose_name=_('Изображение'),
+        default='gallery/default.png',
+        blank=True,
+    )
+    description = models.TextField(
+        verbose_name=_('Описание изображения'),
+    )
+
+    class Meta:
+        verbose_name = _('Изображение')
+        verbose_name_plural = _('Галерея')
+
+    def __str__(self) -> str:
+        return f'Image#{self.pk}'
