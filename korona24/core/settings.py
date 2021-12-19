09465349d13993368bccba3f09751a08870cd0e0
Alexey Bobrov <bobrov.bobrov-alex@yandex.ru>
2021-12-19 20:30:07 +0700

    Templated services menu on main page. Added middleware for that.

--- korona24/core/settings.py
+++ korona24/core/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'visual_version_handler.middleware.visually_impaired',
+                'services.middleware.get_all_services',
             ],
         },
     },
